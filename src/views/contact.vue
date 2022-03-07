fd0711aaba688d6a3efbb3dc298508406aa69362
nandadianaaa <93563135+nandadianaaa@users.noreply.github.com>
2022-03-07 13:13:22 +0700

    Add files via upload

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,22 +1,28 @@
 <template>
-  <div  class="contact">
-
- 
-    <h1>  <img alt="Vue "  src="../assets/peta.png"></h1>
-  
- 
-  
-
- </div>
- 
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Contact</h1>
+        <address class="text-left">
+          Jabar <br>
+          Tasik <br>
+          Indonesia
+        </address>
+  <Peta />
+      </div>
+      <!-- ieu diisian ku component form contact -->
+      <FormContact />
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {
-    
+import Peta from '../components/Peta.vue'
+import FormContact from '../components/FormContact.vue'
+export default {
+  components: {
+    Peta,
+    FormContact,
   }
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>
